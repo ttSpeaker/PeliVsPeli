d9b98642769da99ee6598df7eee35faff4e29971
ttSpeaker <js.serruya@gmail.com>
2019-01-06 10:19:50 -0300

    Pasando a guia 4 docker dividir en front y back

--- server/conexionDb.js
+++ server/conexionDb.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  port     : 3306,
-  user     : 'root',
-  password : 'password',
-  database : 'competencias'
+  host     : process.env.DB_HOST,
+  port     : process.env.DB_PORT,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE
 });
 
 module.exports = connection;
